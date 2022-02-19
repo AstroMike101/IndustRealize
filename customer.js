77a518e45127985f49a20ee269df858bf6413c1b
Aiden Jennings <aidenjennings@outlook.com>
2022-02-19 14:52:33 -0500

    added flip card

---
+++ customer.js
@@ -0,0 +1,9 @@
+function changeText() {
+    var elem = document.getElementById("flip-card-button");
+    if (elem.innerText == "Hire Me!") {
+        elem.innerHTML = "Hired!";
+    }
+    else {
+        elem.innerHTML = "Hire Me!";
+    }
+}
